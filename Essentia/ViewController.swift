aff50c3162bb6a677957daf0be0a0187f3034db8
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-07-12 20:09:36 +0300

    Initial Commit

---
+++ Essentia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Essentia
+//
+//  Created by Pavlo Boiko on 12.07.18.
+//  Copyright © 2018 Essentia-One. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
