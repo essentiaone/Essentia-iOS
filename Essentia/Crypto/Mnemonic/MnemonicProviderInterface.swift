9ce41e080191e72efa0d14f26b52d7a234b60c61
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-07-19 23:46:39 +0300

    Add mnemonic provider

---
+++ Essentia/Crypto/Mnemonic/MnemonicProviderInterface.swift
@@ -0,0 +1,13 @@
+//
+//  MnemonicProviderInterface.swift
+//  Essentia
+//
+//  Created by Pavlo Boiko on 19.07.18.
+//  Copyright © 2018 Essentia-One. All rights reserved.
+//
+
+import Foundation
+
+protocol MnemonicProviderInterface {
+    func generateMnemonic() -> [String]
+}
