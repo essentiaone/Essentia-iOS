cc8ffa24c627b632c38b60b00dc3ec29cf4f768e
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-10-16 17:49:14 +0300

    Create select wallet pop up

---
+++ Essentia/UserCases/Wallet/Controllers/AddWallet/SelectWalletPopUp.swift
@@ -0,0 +1,25 @@
+//
+//  SelectWalletPopUp.swift
+//  Essentia
+//
+//  Created by Pavlo Boiko on 10/16/18.
+//  Copyright © 2018 Essentia-One. All rights reserved.
+//
+
+import Foundation
+
+final class SelectWalletPopUp: BaseTablePopUpController {
+    init(wallets: [WalletInterface], didSelect: (WalletInterface) -> Void) {
+        let state: [TableComponent] = [
+
+        ]
+        super.init(position: .bottom, state: state)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
