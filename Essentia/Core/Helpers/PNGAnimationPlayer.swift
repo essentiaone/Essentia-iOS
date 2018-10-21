e30b2edd60b1d1f175e9c859af96e8adf4ffa9de
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-10-22 02:44:21 +0300

    Correct images names, add PNG animation player

---
+++ Essentia/Core/Helpers/PNGAnimationPlayer.swift
@@ -0,0 +1,25 @@
+//
+//  PNGAnimationPlayer.swift
+//  Essentia
+//
+//  Created by Pavlo Boiko on 10/22/18.
+//  Copyright © 2018 Essentia-One. All rights reserved.
+//
+
+import Foundation
+
+enum PNGAnimation: String {
+    case Exchange
+    case ExchangeEss
+    case Secured
+    case Securing0to30
+    case Securing30to70
+    case Securing70to99
+    case SendTransaction
+}
+
+class PNGAnimationPlayer {
+    init(animation: PNGAnimation) {
+        
+    }
+}
