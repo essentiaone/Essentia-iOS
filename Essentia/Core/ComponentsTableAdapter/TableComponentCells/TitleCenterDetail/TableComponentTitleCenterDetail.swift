0f480c86113441fd4996d118d18f6459e2182dad
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-10-31 01:48:28 +0200

    Title text field component

---
+++ Essentia/Core/ComponentsTableAdapter/TableComponentCells/TitleCenterDetail/TableComponentTitleCenterDetail.swift
@@ -0,0 +1,14 @@
+//
+//  TableComponentTitleCenterDetail.swift
+//  Essentia
+//
+//  Created by Pavlo Boiko on 10/30/18.
+//  Copyright © 2018 Essentia-One. All rights reserved.
+//
+
+import UIKit
+
+class TableComponentTitleCenterDetail: UITableViewCell, NibLoadable {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+}
