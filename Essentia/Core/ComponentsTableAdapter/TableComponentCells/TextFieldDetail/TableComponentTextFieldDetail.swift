7da87d184ab402ec888fcc4380c66d58a13dce92
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-11-02 01:51:25 +0200

    Add text field title with detail component

---
+++ Essentia/Core/ComponentsTableAdapter/TableComponentCells/TextFieldDetail/TableComponentTextFieldDetail.swift
@@ -0,0 +1,14 @@
+//
+//  TableComponentTextFieldDetail.swift
+//  Essentia
+//
+//  Created by Pavlo Boiko on 11/2/18.
+//  Copyright © 2018 Essentia-One. All rights reserved.
+//
+
+import UIKit
+
+class TableComponentTextFieldDetail: UITableViewCell, NibLoadable {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var detailLabel: UILabel!
+}
