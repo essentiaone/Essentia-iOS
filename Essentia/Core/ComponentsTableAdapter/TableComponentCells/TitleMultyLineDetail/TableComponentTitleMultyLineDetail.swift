ed93cdaba11a4ac6b13f5f70a5c4885f600fb72e
impul <pavlo.bojkoo@gmail.com>
2018-11-29 16:45:33 +0200

    Create Multyple lines field component

---
+++ Essentia/Core/ComponentsTableAdapter/TableComponentCells/TitleMultyLineDetail/TableComponentTitleMultyLineDetail.swift
@@ -0,0 +1,9 @@
+//
+//  TableComponentTitleMultyLineDetail.swift
+//  Essentia
+//
+//  Created by Pavlo Boiko on 11/29/18.
+//  Copyright © 2018 Essentia-One. All rights reserved.
+//
+
+import Foundation
