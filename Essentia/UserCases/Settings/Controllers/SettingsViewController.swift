4b55d0aaceec0a02c4e03adc73fa9b1749448267
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-12-21 11:13:10 +0200

    Remove login method from settings

--- Essentia/UserCases/Settings/Controllers/SettingsViewController.swift
+++ Essentia/UserCases/Settings/Controllers/SettingsViewController.swift
@@ -83,9 +83,8 @@ class SettingsViewController: BaseTableAdapterController {
                               title: LS("Settings.Security"),
                               detail: "",
                               action: securityAction),
-             .separator(inset: Constants.separatorInset)]
-             + loginMetodState +
-             [.empty(height: 16, background: colorProvider.settingsBackgroud),
+             .separator(inset: Constants.separatorInset),
+             .empty(height: 16, background: colorProvider.settingsBackgroud),
              .descriptionWithSize(aligment: .left,
                                   fontSize: 13,
                                   title: LS("Settings.Title.Community"),
