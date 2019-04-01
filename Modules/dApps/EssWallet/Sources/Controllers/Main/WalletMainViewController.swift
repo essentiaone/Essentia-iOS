18e02c44976c2db6450476a703688d6ff3fd3ce6
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2019-04-02 01:40:11 +0300

    Refactor main screen stete

--- Modules/dApps/EssWallet/Sources/Controllers/Main/WalletMainViewController.swift
+++ Modules/dApps/EssWallet/Sources/Controllers/Main/WalletMainViewController.swift
@@ -137,32 +137,31 @@ public class WalletMainViewController: BaseTableAdapterController {
     // MARK: - State builders
     func buildSection(title: String, wallets: [ViewWalletInterface]) -> [TableComponent] {
         guard !wallets.isEmpty else { return [] }
-        var sectionState: [TableComponent] = []
-        sectionState.append(.empty(height: 10, background: colorProvider.settingsBackgroud))
-        sectionState.append(.descriptionWithSize(aligment: .left,
-                                                 fontSize: 14,
-                                                 title: title,
-                                                 background: colorProvider.settingsBackgroud,
-                                                 textColor: colorProvider.appDefaultTextColor))
-        sectionState.append(.empty(height: 10, background: colorProvider.settingsBackgroud))
-        sectionState.append(contentsOf: buildStateForWallets(wallets))
-        return sectionState
+        return [
+            .empty(height: 10, background: colorProvider.settingsBackgroud),
+            .descriptionWithSize(aligment: .left,
+                                 fontSize: 14,
+                                 title: title,
+                                 background: colorProvider.settingsBackgroud,
+                                 textColor: colorProvider.appDefaultTextColor),
+            .empty(height: 10, background: colorProvider.settingsBackgroud)] +
+            buildStateForWallets(wallets)
     }
     
     func buildStateForWallets(_ wallets: [ViewWalletInterface]) -> [TableComponent] {
-        let assetsState: [[TableComponent]] = wallets.map { wallet in
-            let oneAssetState: [TableComponent] =
-                [ .assetBalance(imageUrl: wallet.iconUrl,
-                                title: wallet.name,
-                                value: wallet.formattedBalanceInCurrentCurrencyWithSymbol,
-                                currencyValue: wallet.formattedBalanceWithSymbol.uppercased(),
-                                action: { [unowned self] in
-                                    self.showWalletDetail(for: wallet)}),
-                  .separator(inset: .zero)
-                    ] as [TableComponent]
-            return oneAssetState
-            }
-        return assetsState |> concat
+        return wallets |> buildAsetState |> concat
+    }
+    
+    private func buildAsetState(_ wallet: ViewWalletInterface) -> [TableComponent] {
+        return 
+            [ .assetBalance(imageUrl: wallet.iconUrl,
+                            title: wallet.name,
+                            value: wallet.formattedBalanceInCurrentCurrencyWithSymbol,
+                            currencyValue: wallet.formattedBalanceWithSymbol.uppercased(),
+                            action: { [unowned self] in
+                                self.showWalletDetail(for: wallet)}),
+              .separator(inset: .zero)
+        ]
     }
     
     // MARK: - Cash
